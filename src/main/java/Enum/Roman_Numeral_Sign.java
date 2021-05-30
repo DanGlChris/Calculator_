24278d83d9e80339d7e2805068a59e94f64fcfb9
DanGlChris <dagloxkankwanda@gmail.com>
2021-05-30 19:16:31 +0300

    Тест 1цй верция

--- src/main/java/Enum/Roman_Numeral_Sign.java
+++ src/main/java/Enum/Roman_Numeral_Sign.java
@@ -46,5 +46,17 @@ public enum Roman_Numeral_Sign {
             }
         } return numerals;
     }
+    private static int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    private static String[] romanLiterals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    public static String Roman_value_of(int num){
+        String roman_value = "";
+        for(int i=0; i<values.length; i++) {
+            while(num >= values[i]) {
+                num -= values[i];
+                roman_value += romanLiterals[i];
+            }
+        }
+        return roman_value;
+    }
     
 }
