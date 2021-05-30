369dbd916a03c75b71ce6f63178eb70794bb5428
DanGlChris <dagloxkankwanda@gmail.com>
2021-05-30 19:15:27 +0300

    Тест 1цй верция

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,6 +2,8 @@
 import Enum.Operations;
 import Enum.Roman_Numeral_Sign;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,25 +21,30 @@ public class Calculator {
      * @param args the command line arguments
      */
     private static String[] inputs = new String[3]; // {элемент a, операция, элемент b}
+    private static String result = "";
     
     public static void main(String[] args) {     
-        char[] entry = "MDCCCXXVI".toCharArray();
+        /*char[] entry = "LXIX".toCharArray();
         
         Roman_Numeral_Sign[] Roman_value_a = Roman_Numeral_Sign.convert_to_numeral(entry);
         
-        Roman_numeral a = new Roman_numeral(Roman_value_a);
-        //System.out.println(Roman_value_a[0].isUnity());
-        System.out.println(String.copyValueOf(entry) + " = " +a.getValue());
+        Roman_numeral a;*/
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            boolean test = false;
         
-        /*Scanner scanner = new Scanner(System.in);
-        boolean test = false;
+            while(!test){
+                System.out.println("Input:");
+                String entry = scanner.next();
+                test = Verify_Entry(entry);
+                test = Solve(inputs);            
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Calculator.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        while(!test){
-            System.out.println("Input:");
-            String entry = scanner.next();
-            test = Verify_Entry(entry);
-            test = Verify_Element(inputs);            
-        }*/
+        /**/
     }
     /**
      * Функция помогает узнать если вход содержает 
@@ -75,7 +82,7 @@ public class Calculator {
      * @param entry Массив из 3 числа
      * @return 
      */
-    private static boolean Verify_Element(String[] entry){
+    private static boolean Solve(String[] entry){
         test_roman = false;        
         roman_value = false;
         int_value_a = 0;
@@ -87,31 +94,50 @@ public class Calculator {
             try {                
                 Roman_numeral a = new Roman_numeral(Roman_value_a);
                 Roman_numeral b = new Roman_numeral(Roman_value_b);
-                System.out.println("That work! " + a.getValue()+ " " +b.getValue());
+                if(a.getValue()>10 || b.getValue()>10) return false;
+                else{
+                    System.out.println("Output : ");
+                    switch(inputs[1].charAt(0)){
+                        case '+' : result = a.add_to(b); break;
+                        case '-' : result = a.substract_to(b); break;
+                        case '*' : result = a.multiply_by(b); break;
+                        case '/' : result = a.divide_by(b); break;
+                        default: result = ""; break;
+                    }
+                    System.out.println(result); return true;
+                }                
             } catch (Exception e) {
-                System.err.println("Errrrrrrrrrrrrorrr");
+                return false;
             }
-            return true;
         }else{    //Значит что один или два из вдух свойства не соблюдается
-            System.out.println("Romans lost the war...");
             try{                
                 int_value_a = Integer.parseInt(entry[0]);
                 int_value_b = Integer.parseInt(entry[2]);
-                if(int_value_a>10 || int_value_a<0){
-                    System.out.println("But there are more than 10 persons");                    
-                    return false;
+                if(int_value_a <0 || int_value_a >10 || int_value_b <0 || int_value_b>10) return false;
+                else{
+                    System.out.println("Output : ");
+                    switch(inputs[1].charAt(0)){
+                        case '+' : result = String.valueOf((int)(int_value_a + int_value_b)); break;
+                        case '-' :{
+                            result = int_value_a>int_value_b? String.valueOf((int)(int_value_a - int_value_b)):
+                                    "-" + String.valueOf((int)(int_value_b - int_value_a));
+                        } break;
+                        case '*' : result = String.valueOf((int)(int_value_a * int_value_b)); break;
+                        case '/' : {
+                            if(int_value_b==0) throw new ArithmeticException();
+                            else result = String.valueOf((int)(int_value_a / int_value_b));
+                        } break;
+                        default: result = ""; break;
+                    }
+                    System.out.println(result); return true;                    
                 }
-                if(int_value_b>10 || int_value_b<0){
-                    System.out.println("But there are more than 10 persons");       
-                    return false;
-                }
-                
-                return true;
                 
             }catch (NumberFormatException ex){
                 return false;
             }
-            
+            catch (ArithmeticException ex){
+                return false;
+            }
         }
     }
     
